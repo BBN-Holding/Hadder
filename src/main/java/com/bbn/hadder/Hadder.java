c0041831c82ce9023c7de8d408fac22677cdfd76
Hax <arzberger52@gmail.com>
2019-10-25 10:24:52 +0200

    First real Commit

---
+++ src/main/java/com/bbn/hadder/Hadder.java
@@ -0,0 +1,43 @@
+package com.bbn.hadder;
+
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+import org.json.JSONObject;
+
+import javax.security.auth.login.LoginException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Hadder {
+
+    public static void main(String[] args) {
+
+        File jsonfile = new File("./config.json");
+        if (!jsonfile.exists()) {
+            System.err.println("No Config File Found!");
+            System.exit(1);
+        }
+
+        JSONObject config = null;
+        try {
+            config = new JSONObject(new String(Files.readAllBytes(Paths.get(jsonfile.toURI()))));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        DefaultShardManagerBuilder builder = new DefaultShardManagerBuilder();
+
+        builder.setShardsTotal(1);
+        builder.setToken(config.getString("Token"));
+
+        try {
+            ShardManager shardManager = builder.build();
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
+
+    }
+    
+}
