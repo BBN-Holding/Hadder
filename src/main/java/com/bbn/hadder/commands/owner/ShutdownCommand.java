499913e78908907c841a085b766f3ed14a0dc53d
GregTCLTK <gregtcltk@gmail.com>
2019-10-27 11:04:18 +0100

    ShutdownCommand

---
+++ src/main/java/com/bbn/hadder/commands/owner/ShutdownCommand.java
@@ -0,0 +1,26 @@
+package com.bbn.hadder.commands.owner;
+
+/*
+ * @author Skidder / GregTCLTK
+ */
+
+import com.bbn.hadder.Rethink;
+import com.bbn.hadder.commands.Command;
+import com.bbn.hadder.utils.MessageEditor;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class ShutdownCommand implements Command {
+    @Override
+    public void executed(String[] args, MessageReceivedEvent event) {
+        if (event.getAuthor().getId().equals("477141528981012511") || event.getAuthor().getId().equals("261083609148948488")) {
+            EmbedBuilder builder = new EmbedBuilder();
+            event.getTextChannel().sendMessage(new MessageEditor().setDefaultSettings(MessageEditor.Messagetype.INFO, builder).setTitle("Shutdown").build()).queue();
+            event.getJDA().shutdown();
+            Rethink.disconnect();
+        } else {
+            EmbedBuilder builder = new EmbedBuilder();
+            event.getTextChannel().sendMessage(new MessageEditor().setDefaultSettings(MessageEditor.Messagetype.NO_PERMISSION, builder).build()).queue();
+        }
+    }
+}
