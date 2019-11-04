fcb33ef5c7e44a298a58ba6cee38d917dfe8544e
Hax <arzberger52@gmail.com>
2019-11-04 18:15:51 +0100

    Updated some things

--- src/main/java/com/bbn/hadder/listener/MentionListener.java
+++ src/main/java/com/bbn/hadder/listener/MentionListener.java
@@ -1,5 +1,6 @@
 package com.bbn.hadder.listener;
 
+import com.bbn.hadder.Rethink;
 import com.bbn.hadder.utils.MessageEditor;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.ChannelType;
@@ -10,6 +11,12 @@ import javax.annotation.Nonnull;
 
 public class MentionListener extends ListenerAdapter {
 
+    private Rethink rethink;
+
+    public MentionListener(Rethink rethink) {
+        this.rethink = rethink;
+    }
+
     @Override
     public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
         if (event.isFromType(ChannelType.TEXT)) {
@@ -18,7 +25,9 @@ public class MentionListener extends ListenerAdapter {
                         .setTitle("Hello I'm Hadder.")
                         .setAuthor(event.getJDA().getSelfUser().getName(), event.getJDA().getSelfUser().getAvatarUrl(), event.getJDA().getSelfUser().getAvatarUrl())
                         .addField("Users", String.valueOf(event.getJDA().getUsers().size()), false)
-                        .addField("Guilds", String.valueOf(event.getJDA().getGuilds().size()), false);
+                        .addField("Guilds", String.valueOf(event.getJDA().getGuilds().size()), false)
+                        .addField("Prefix (User)", rethink.getUserPrefix(event.getAuthor().getId()), false)
+                        .addField("Prefix (Guild)", rethink.getServerPrefix(event.getGuild().getId()), false);
                 event.getChannel().sendMessage(new MessageEditor().setDefaultSettings(MessageEditor.Messagetype.INFO, builder).build()).queue();
             }
         }
