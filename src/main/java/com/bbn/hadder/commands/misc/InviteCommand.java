6a5b4028dc0a39baa18531bf0e5cea9b4196fa5c
GregTCLTK <gregtcltk@gmail.com>
2019-11-15 15:57:35 +0100

    InviteCommand yk

--- src/main/java/com/bbn/hadder/commands/misc/InviteCommand.java
+++ src/main/java/com/bbn/hadder/commands/misc/InviteCommand.java
@@ -4,6 +4,7 @@ package com.bbn.hadder.commands.misc;
  * @author Skidder / GregTCLTK
  */
 
+import com.bbn.hadder.Hadder;
 import com.bbn.hadder.commands.Command;
 import com.bbn.hadder.commands.CommandEvent;
 import com.bbn.hadder.utils.MessageEditor;
@@ -14,7 +15,7 @@ public class InviteCommand implements Command {
     @Override
     public void executed(String[] args, CommandEvent event) {
         EmbedBuilder builder = new EmbedBuilder();
-        event.getTextChannel().sendMessage(new MessageEditor().setDefaultSettings(MessageEditor.Messagetype.INFO, builder).setTitle("Invite me!").setDescription("[Invite me here!](discordapp.com/oauth2/authorize?client_id=637002314162372639&scope=bot&permissions=470133879)").build()).queue();
+        event.getTextChannel().sendMessage(new MessageEditor().setDefaultSettings(MessageEditor.Messagetype.INFO, builder).setTitle("Invite me!").setDescription("[Invite me here!](discordapp.com/oauth2/authorize?client_id="  + Hadder.shardManager.getGuilds().get(0).getSelfMember().getId() + "&scope=bot&permissions=470133879)").build()).queue();
     }
 
     @Override
