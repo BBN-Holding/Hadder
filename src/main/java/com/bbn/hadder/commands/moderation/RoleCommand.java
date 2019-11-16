7cf1e8769f403df6d2b61ce51fee419ab6ef6cff
GregTCLTK <gregtcltk@gmail.com>
2019-11-17 00:36:56 +0100

    Check perms

--- src/main/java/com/bbn/hadder/commands/moderation/RoleCommand.java
+++ src/main/java/com/bbn/hadder/commands/moderation/RoleCommand.java
@@ -12,57 +12,68 @@ public class RoleCommand implements Command {
 
     @Override
     public void executed(String[] args, CommandEvent event) {
-        switch (args[0].toLowerCase()) {
-            case "add":
-                if (event.getMember().hasPermission(Permission.MANAGE_ROLES)) {
-                    if (event.getMessage().getMentionedMembers().size() > 0 && event.getMessage().getMentionedRoles().size() == 1) {
-                        for (Member member : event.getMessage().getMentionedMembers()) {
-                            if (event.getGuild().getSelfMember().canInteract(member)) {
-                                if (!member.getRoles().contains(event.getMessage().getMentionedRoles().get(0))) {
-                                    event.getGuild().addRoleToMember(member, event.getMessage().getMentionedRoles().get(0))
-                                            .reason("Role Command executed by " + event.getAuthor().getAsTag()).queue();
-                                }
-                            }
-                        }
-                        event.getChannel().sendMessage(
-                                new MessageEditor().setDefaultSettings(MessageEditor.Messagetype.INFO,
-                                        new EmbedBuilder().setTitle("Successfully added all " +
-                                                event.getMessage().getMentionedMembers().size() +
-                                                " Members the " + event.getMessage().getMentionedRoles().get(0).getName() + " Role")).build()).queue();
-                    } else if (event.getMessage().getMentionedRoles().size() > 1) {
-                        for (Role role : event.getMessage().getMentionedRoles()) {
-                            if (role == event.getMessage().getMentionedRoles().get(event.getMessage().getMentionedRoles().size() - 1)) {
-                                if (event.getGuild().getSelfMember().canInteract(role)) {
-                                    int highestrole = 0;
-                                    for (Role testrole : event.getGuild().getSelfMember().getRoles()) {
-                                        if (testrole.getPosition()>highestrole) {
-                                            highestrole=testrole.getPosition();
-                                        }
-                                    }
-                                    for (Member member : event.getGuild().getMembers()) {
-                                        if (member.getRoles().contains(role)) {
+        if (args.length > 0) {
+            if (event.getMember().hasPermission(Permission.MANAGE_ROLES)) {
+                if (event.getGuild().getSelfMember().hasPermission(Permission.MANAGE_ROLES)) {
+                    switch (args[0].toLowerCase()) {
+                        case "add":
+                            if (event.getMessage().getMentionedMembers().size() > 0 && event.getMessage().getMentionedRoles().size() == 1) {
+                                for (Member member : event.getMessage().getMentionedMembers()) {
+                                    if (event.getGuild().getSelfMember().canInteract(member)) {
+                                        if (!member.getRoles().contains(event.getMessage().getMentionedRoles().get(0))) {
                                             event.getGuild().addRoleToMember(member, event.getMessage().getMentionedRoles().get(0))
                                                     .reason("Role Command executed by " + event.getAuthor().getAsTag()).queue();
                                         }
                                     }
                                 }
+                                event.getChannel().sendMessage(
+                                        new MessageEditor().setDefaultSettings(MessageEditor.Messagetype.INFO,
+                                                new EmbedBuilder().setTitle("Successfully added all " +
+                                                        event.getMessage().getMentionedMembers().size() +
+                                                        " Members the " + event.getMessage().getMentionedRoles().get(0).getName() + " Role")).build()).queue();
+                            } else if (event.getMessage().getMentionedRoles().size() > 1) {
+                                for (Role role : event.getMessage().getMentionedRoles()) {
+                                    if (role == event.getMessage().getMentionedRoles().get(event.getMessage().getMentionedRoles().size() - 1)) {
+                                        if (event.getGuild().getSelfMember().canInteract(role)) {
+                                            int highestrole = 0;
+                                            for (Role testrole : event.getGuild().getSelfMember().getRoles()) {
+                                                if (testrole.getPosition() > highestrole) {
+                                                    highestrole = testrole.getPosition();
+                                                }
+                                            }
+                                            for (Member member : event.getGuild().getMembers()) {
+                                                if (member.getRoles().contains(role)) {
+                                                    event.getGuild().addRoleToMember(member, event.getMessage().getMentionedRoles().get(0))
+                                                            .reason("Role Command executed by " + event.getAuthor().getAsTag()).queue();
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
                             }
-                        }
+                            break;
+
+                        case "remove":
+
+
+                            break;
                     }
+                } else {
+                    EmbedBuilder builder = new EmbedBuilder();
+                    event.getTextChannel().sendMessage(new MessageEditor().setDefaultSettings(MessageEditor.Messagetype.NO_SELF_PERMISSION, builder).build()).queue();
                 }
-
-                break;
-
-            case "remove":
-
-
-                break;
+            } else {
+                EmbedBuilder builder = new EmbedBuilder();
+                event.getTextChannel().sendMessage(new MessageEditor().setDefaultSettings(MessageEditor.Messagetype.NO_PERMISSION, builder).build()).queue();
+            }
+        } else {
+            event.getTextChannel().sendMessage("Missing args").queue();
         }
     }
 
     @Override
     public String[] labels() {
-        return new String[]{"addrole", "addroles"};
+        return new String[]{"role", "roles"};
     }
 
     @Override
