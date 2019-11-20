450253106c6b0c78a3937160943ad8017642594c
GregTCLTK <gregtcltk@gmail.com>
2019-11-20 10:16:23 -0800

    Perms check

--- src/main/java/com/bbn/hadder/commands/moderation/RulesCommand.java
+++ src/main/java/com/bbn/hadder/commands/moderation/RulesCommand.java
@@ -6,12 +6,26 @@ package com.bbn.hadder.commands.moderation;
 
 import com.bbn.hadder.commands.Command;
 import com.bbn.hadder.commands.CommandEvent;
+import com.bbn.hadder.utils.MessageEditor;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
 
 public class RulesCommand implements Command {
 
     @Override
     public void executed(String[] args, CommandEvent event) {
+        if (event.getMember().hasPermission(Permission.MANAGE_SERVER)) {
+            if (event.getGuild().getSelfMember().hasPermission(Permission.MANAGE_ROLES)) {
 
+                
+            } else {
+                EmbedBuilder builder = new EmbedBuilder();
+                event.getTextChannel().sendMessage(new MessageEditor().setDefaultSettings(MessageEditor.MessageType.NO_SELF_PERMISSION, builder).build()).queue();
+            }
+        } else {
+            EmbedBuilder builder = new EmbedBuilder();
+            event.getTextChannel().sendMessage(new MessageEditor().setDefaultSettings(MessageEditor.MessageType.NO_PERMISSION, builder).build()).queue();
+        }
     }
 
     @Override
@@ -21,7 +35,7 @@ public class RulesCommand implements Command {
 
     @Override
     public String description() {
-        return "Setup the rules on your server";
+        return "Setup the rules on your Discord server";
     }
 
     @Override
