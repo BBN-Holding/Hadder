d414539814784b535f12c079dfc6e8bad4dc4306
GregTCLTK <gregtcltk@gmail.com>
2019-12-01 09:53:22 +0100

    Add BotList.Space

--- src/main/java/com/bbn/hadder/core/Config.java
+++ src/main/java/com/bbn/hadder/core/Config.java
@@ -137,4 +137,8 @@ public class Config {
     public String getDiscordBotsToken() {
         return config.getJSONObject("Tokens").getString("DiscordBots");
     }
+
+    public String getBotListSpaceToken() {
+        return config.getJSONObject("Tokens").getString("BotListSpace");
+    }
 }
