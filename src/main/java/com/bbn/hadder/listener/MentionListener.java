597953737190b311c6056757df1d29a455709729
Hax <arzberger52@gmail.com>
2019-12-12 21:47:32 +0100

    Fixed the MentionListener

--- src/main/java/com/bbn/hadder/listener/MentionListener.java
+++ src/main/java/com/bbn/hadder/listener/MentionListener.java
@@ -7,6 +7,7 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
+import java.util.Random;
 
 public class MentionListener extends ListenerAdapter {
 
@@ -18,7 +19,8 @@ public class MentionListener extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
-        if (event.isFromType(ChannelType.TEXT) && event.getMessage().getContentRaw().equals(event.getGuild().getSelfMember().getAsMention())) {
+        if (event.isFromType(ChannelType.TEXT) && (event.getMessage().getContentRaw().equals(event.getGuild().getSelfMember().getAsMention())||
+                event.getMessage().getContentRaw().equals(event.getGuild().getSelfMember().getAsMention().replace("@", "@!")))) {
             event.getChannel().sendMessage(new MessageEditor().setDefaultSettings(MessageEditor.MessageType.INFO)
                     .setTitle("Hello I'm Hadder.")
                     .setAuthor(event.getJDA().getSelfUser().getName(), event.getJDA().getSelfUser().getAvatarUrl(), event.getJDA().getSelfUser().getAvatarUrl())
