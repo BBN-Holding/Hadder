3e1de81eb82aab656f5770c4f69ea52f0d2e1833
GregTCLTK <gregtcltk@gmail.com>
2019-12-18 20:11:54 +0100

    ;)

--- src/main/java/com/bbn/hadder/commands/Perm.java
+++ src/main/java/com/bbn/hadder/commands/Perm.java
@@ -10,58 +10,58 @@ public enum Perm {
 
     BOT_OWNER() {
         @Override
-        public boolean check(CommandEvent commandEvent) {
-            return commandEvent.getConfig().getOwners().contains(commandEvent.getAuthor().getIdLong());
+        public boolean check(CommandEvent event) {
+            return event.getConfig().getOwners().contains(event.getAuthor().getIdLong());
         }
     },
     MANAGE_MESSAGES {
         @Override
-        public boolean check(CommandEvent commandEvent) {
-            return commandEvent.getMember().hasPermission(Permission.MESSAGE_MANAGE);
+        public boolean check(CommandEvent event) {
+            return event.getMember().hasPermission(Permission.MESSAGE_MANAGE);
         }
     },
     EMBED_MESSAGES {
         @Override
-        public boolean check(CommandEvent commandEvent) {
-            return commandEvent.getMember().hasPermission(Permission.MESSAGE_EMBED_LINKS);
+        public boolean check(CommandEvent event) {
+            return event.getMember().hasPermission(Permission.MESSAGE_EMBED_LINKS);
         }
     },
     BAN_MEMBERS {
         @Override
-        public boolean check(CommandEvent commandEvent) {
-            return commandEvent.getMember().hasPermission(Permission.BAN_MEMBERS);
+        public boolean check(CommandEvent event) {
+            return event.getMember().hasPermission(Permission.BAN_MEMBERS);
         }
     },
     KICK_MEMBERS {
         @Override
-        public boolean check(CommandEvent commandEvent) {
-            return commandEvent.getMember().hasPermission(Permission.KICK_MEMBERS);
+        public boolean check(CommandEvent event) {
+            return event.getMember().hasPermission(Permission.KICK_MEMBERS);
         }
     },
     MANAGE_SERVER {
         @Override
-        public boolean check(CommandEvent commandEvent) {
-            return commandEvent.getMember().hasPermission(Permission.MANAGE_SERVER);
+        public boolean check(CommandEvent event) {
+            return event.getMember().hasPermission(Permission.MANAGE_SERVER);
         }
     },
     MANAGE_ROLES {
         @Override
-        public boolean check(CommandEvent commandEvent) {
-            return commandEvent.getMember().hasPermission(Permission.MANAGE_ROLES);
+        public boolean check(CommandEvent event) {
+            return event.getMember().hasPermission(Permission.MANAGE_ROLES);
         }
     },
     CHANGE_NICKNAME {
         @Override
-        public boolean check(CommandEvent commandEvent) {
-            return commandEvent.getMember().hasPermission(Permission.NICKNAME_CHANGE);
+        public boolean check(CommandEvent event) {
+            return event.getMember().hasPermission(Permission.NICKNAME_CHANGE);
         }
     },
     ADMIN_PERMISSIONS {
         @Override
-        public boolean check(CommandEvent commandEvent) {
-            return commandEvent.getMember().hasPermission(Permission.ADMINISTRATOR);
+        public boolean check(CommandEvent event) {
+            return event.getMember().hasPermission(Permission.ADMINISTRATOR);
         }
     };
 
-    public abstract boolean check(CommandEvent commandEvent);
+    public abstract boolean check(CommandEvent event);
 }
