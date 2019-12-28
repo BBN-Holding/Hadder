8d19b9ae07a7557751216144587713485c422c03
GregTCLTK <gregtcltk@gmail.com>
2019-12-28 14:14:45 +0100

    Info Command

--- src/main/java/com/bbn/hadder/commands/music/InfoCommand.java
+++ src/main/java/com/bbn/hadder/commands/music/InfoCommand.java
@@ -11,19 +11,16 @@ import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 
 public class InfoCommand implements Command {
 
-    private static final String CD = "\uD83D\uDCBF";
-    private static final String MIC = "\uD83C\uDFA4";
-    private static final String QUEUE_DESCRIPTION = "%s **|>**  %s\n%s\n%s %s\n%s";
-
     @Override
     public void executed(String[] args, CommandEvent event) {
-        if (event.getAudioManager().hasPlayer(event.getGuild()) && event.getAudioManager().getPlayer(event.getGuild()).getPlayingTrack() == null) {
+        if (event.getAudioManager().hasPlayer(event.getGuild()) && event.getAudioManager().getPlayer(event.getGuild()).getPlayingTrack() != null) {
             AudioTrack track = event.getAudioManager().getPlayer(event.getGuild()).getPlayingTrack();
-            event.getTextChannel().sendMessage(event.getMessageEditor().getMessage(MessageEditor.MessageType.INFO, "", "", "", "").build()).queue();
-            event.getTextChannel().sendMessage("Track Info" + String.format(QUEUE_DESCRIPTION, CD, event.getAudioManager().getOrNull(track.getInfo().title),
-                    "\n\u23F1 **|>** `[ " + event.getAudioManager().getTimestamp(track.getPosition()) + " / " + event.getAudioManager().getTimestamp(track.getInfo().length) + " ]`",
-                    "\n" + MIC, event.getAudioManager().getOrNull(track.getInfo().author),
-                    "\n\uD83C\uDFA7 **|>**  " + "")).queue();
+            event.getTextChannel().sendMessage(event.getMessageEditor().getMessage(MessageEditor.MessageType.INFO,
+                    "commands.music.info.success.title", "")
+                    .setAuthor(track.getInfo().author)
+                    .addField("Title", track.getInfo().title, true)
+                    .addField("Progress", "`[ " + event.getAudioManager().getTimestamp(track.getPosition()) + " / " + event.getAudioManager().getTimestamp(track.getInfo().length) + " ]`", false)
+                    .build()).queue();
         } else {
             event.getTextChannel().sendMessage(event.getMessageEditor().getMessage(MessageEditor.MessageType.ERROR,
                     "commands.music.info.error.title",
@@ -38,7 +35,7 @@ public class InfoCommand implements Command {
 
     @Override
     public String description() {
-        return "Shows information about the playing song";
+        return "commands.music.info.help.description";
     }
 
     @Override
--- src/main/java/com/bbn/hadder/commands/owner/EvalCommand.java
+++ src/main/java/com/bbn/hadder/commands/owner/EvalCommand.java
@@ -44,7 +44,7 @@ public class EvalCommand implements Command {
             engine.put("author", event.getAuthor());
             engine.put("member", event.getMember());
             engine.put("self", event.getGuild().getSelfMember());
-            engine.put("audio", new AudioManager());
+            engine.put("audio", event.getAudioManager());
             engine.put("out", System.out);
 
             ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
@@ -55,12 +55,12 @@ public class EvalCommand implements Command {
                 Object out;
 
                 try {
-                    String script = "";
+                    StringBuilder script = new StringBuilder();
                     for (int i = 0; i < args.length; i++) {
                         args[i] = args[i].replace("```java", "").replace("```", "");
-                        script += i == args.length - 1 ? args[i] : args[i] + " ";
+                        script.append(i == args.length - 1 ? args[i] : args[i] + " ");
                     }
-                    out = engine.eval(script);
+                    out = engine.eval(script.toString());
 
                     event.getTextChannel().sendMessage(event.getMessageEditor()
                             .getMessage(MessageEditor.MessageType.INFO, "commands.owner.eval.success.title", "")
