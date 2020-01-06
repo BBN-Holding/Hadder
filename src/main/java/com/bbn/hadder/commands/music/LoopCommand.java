c687cef65da0e299303b4517cec019c31c7bd7e3
GregTCLTK <gregtcltk@gmail.com>
2020-01-06 18:26:59 +0100

    Error messages

--- src/main/java/com/bbn/hadder/commands/music/LoopCommand.java
+++ src/main/java/com/bbn/hadder/commands/music/LoopCommand.java
@@ -2,6 +2,7 @@ package com.bbn.hadder.commands.music;
 
 import com.bbn.hadder.commands.Command;
 import com.bbn.hadder.commands.CommandEvent;
+import com.bbn.hadder.utils.MessageEditor;
 
 /**
  * @author Skidder / GregTCLTK
@@ -11,7 +12,20 @@ public class LoopCommand implements Command {
 
     @Override
     public void executed(String[] args, CommandEvent event) {
-
+        if (event.getAudioManager().hasPlayer(event.getGuild()) && event.getAudioManager().getPlayer(event.getGuild()).getPlayingTrack() != null) {
+            if (event.getMember().getVoiceState().inVoiceChannel() && event.getGuild().getSelfMember().getVoiceState().inVoiceChannel() && event.getGuild().getSelfMember().getVoiceState().getChannel().equals(event.getMember().getVoiceState().getChannel())) {
+                
+            } else {
+                event.getTextChannel().sendMessage(event.getMessageEditor().getMessage(MessageEditor.MessageType.ERROR,
+                        "commands.music.loop.error.connected.title",
+                        "commands.music.loop.error.connected.description")
+                        .build()).queue();
+            }
+        } else {
+            event.getTextChannel().sendMessage(event.getMessageEditor().getMessage(MessageEditor.MessageType.ERROR,
+                    "commands.music.info.error.title",
+                    "commands.music.info.error.description").build()).queue();
+        }
     }
 
     @Override
