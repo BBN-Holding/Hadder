d07e8deefdeb204c4a010b0443f1b6f28bd45024
GregTCLTK <gregtcltk@gmail.com>
2020-01-16 19:51:18 +0100

    Not needed

--- src/main/java/com/bbn/hadder/core/Config.java
+++ src/main/java/com/bbn/hadder/core/Config.java
@@ -103,10 +103,6 @@ public class Config {
         return config.getJSONObject("Tokens").getString("GitHub");
     }
 
-    public String getWakaTimeToken() {
-        return config.getJSONObject("Tokens").getString("WakaTime");
-    }
-
     public String getMythicalBotListToken() {
         return config.getJSONObject("Tokens").getString("MythicalBotList");
     }
