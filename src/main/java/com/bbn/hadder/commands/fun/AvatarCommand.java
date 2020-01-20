6b09db43d9f2f32a782b8ee543282b8e959f3b8d
GregTCLTK <gregtcltk@gmail.com>
2020-01-20 18:16:05 +0100

    More error msgs

--- src/main/java/com/bbn/hadder/commands/fun/AvatarCommand.java
+++ src/main/java/com/bbn/hadder/commands/fun/AvatarCommand.java
@@ -1,13 +1,14 @@
 package com.bbn.hadder.commands.fun;
 
-/*
- * @author Skidder / GregTCLTK
- */
-
 import com.bbn.hadder.commands.Command;
 import com.bbn.hadder.commands.CommandEvent;
 import com.bbn.hadder.utils.MessageEditor;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
+
+/**
+ * @author Skidder / GregTCLTK
+ */
 
 public class AvatarCommand implements Command {
 
@@ -37,18 +38,26 @@ public class AvatarCommand implements Command {
                     .setImage(member.getUser().getAvatarUrl())
                     .setFooter(member.getUser().getAsTag())
                     .build()).queue();
-        } else if (args[0].length() == 18){
-            Member member = event.getGuild().getMemberById(args[0]);
-            event.getTextChannel().sendMessage(
-                    event.getMessageEditor().getMessage(
-                            MessageEditor.MessageType.INFO,
-                            "commands.fun.avatar.success.title",
-                            member.getUser().getAsTag(),
-                            "",
-                            "")
-                    .setImage(member.getUser().getAvatarUrl())
-                    .setFooter(member.getUser().getAsTag())
-                    .build()).queue();
+        } else if (args[0].length() == 18) {
+            try {
+                User u = event.getGuild().getMemberById(args[0]).getUser();
+                event.getTextChannel().sendMessage(
+                        event.getMessageEditor().getMessage(
+                                MessageEditor.MessageType.INFO,
+                                "commands.fun.avatar.success.title",
+                                u.getAsTag(),
+                                "",
+                                "")
+                                .setImage(u.getAvatarUrl())
+                                .setFooter(u.getAsTag())
+                                .build()).queue();
+            } catch (NullPointerException e) {
+                event.getTextChannel().sendMessage(event.getMessageEditor().getMessage(MessageEditor.MessageType.ERROR,
+                        "commands.fun.avatar.error.title",
+                        "commands.fun.avatar.error.description"
+                ).build()).queue();
+            }
+
         } else {
             event.getHelpCommand().sendHelp(this, event);
         }
