0eab70c78d300c4c555ae8456d8d716b5a9c1480
GregTCLTK <gregtcltk@gmail.com>
2020-01-20 19:11:43 +0100

    Integrate it lol2

--- src/main/java/com/bbn/hadder/commands/nsfw/AnalCommand.java
+++ src/main/java/com/bbn/hadder/commands/nsfw/AnalCommand.java
@@ -7,39 +7,19 @@ package com.bbn.hadder.commands.nsfw;
 import com.bbn.hadder.commands.Command;
 import com.bbn.hadder.commands.CommandEvent;
 import com.bbn.hadder.utils.MessageEditor;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import org.json.JSONObject;
-
-import java.io.IOException;
+import com.bbn.hadder.utils.Request;
 
 public class AnalCommand implements Command {
 
     @Override
     public void executed(String[] args, CommandEvent e) {
         if (e.getTextChannel().isNSFW()) {
-
-            OkHttpClient caller = new OkHttpClient();
-            Request request = new Request.Builder().url("https://api.nekos.dev/api/v3/images/nsfw/gif/anal/").build();
-
-            try {
-                Response response = caller.newCall(request).execute();
-                JSONObject json = new JSONObject(response.body().string());
-                JSONObject data = json.getJSONObject("data");
-                JSONObject response1 = data.getJSONObject("response");
-                String url = response1.toString().replace("{\"url\":\"", "");
-
-                e.getTextChannel().sendMessage(e.getMessageEditor().getMessage(MessageEditor.MessageType.INFO)
-                        .setAuthor(e.getMessageEditor().getTerm("commands.nsfw.gif.error.title"), url.replace("\"}", ""))
-                        .setImage(url.replace("\"}", ""))
-                        .setFooter("Anal")
-                        .build()).queue();
-
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-
+            String url = Request.get("https://api.nekos.dev/api/v3/images/nsfw/gif/anal/");
+            e.getTextChannel().sendMessage(e.getMessageEditor().getMessage(MessageEditor.MessageType.INFO)
+                    .setAuthor(e.getMessageEditor().getTerm("commands.nsfw.gif.error.title"), url)
+                    .setImage(url)
+                    .setFooter("Anal")
+                    .build()).queue();
         } else {
             e.getTextChannel().sendMessage(e.getMessageEditor().getMessage(MessageEditor.MessageType.NO_NSFW).build()).queue();
         }
