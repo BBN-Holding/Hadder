1aec456d89670641f1fe010a52f7479efd2e04f7
GregTCLTK <gregtcltk@gmail.com>
2020-01-20 20:21:27 +0100

    Erotic

--- src/main/java/com/bbn/hadder/commands/nsfw/EroticCommand.java
+++ src/main/java/com/bbn/hadder/commands/nsfw/EroticCommand.java
@@ -7,11 +7,7 @@ package com.bbn.hadder.commands.nsfw;
 import com.bbn.hadder.commands.Command;
 import com.bbn.hadder.commands.CommandEvent;
 import com.bbn.hadder.utils.MessageEditor;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
-import java.io.IOException;
+import com.bbn.hadder.utils.Request;
 
 public class EroticCommand implements Command {
 
@@ -19,23 +15,13 @@ public class EroticCommand implements Command {
     public void executed(String[] args, CommandEvent e) {
         if (e.getTextChannel().isNSFW()) {
 
-            OkHttpClient caller = new OkHttpClient();
-            Request request = new Request.Builder().url("https://nekos.life/api/v2/img/erok").build();
+            String url = Request.get("https://nekos.life/api/v2/img/erok");
 
-            try {
-
-                Response response = caller.newCall(request).execute();
-                String url = response.body().string().replace("{\"url\":\"", "");
-
-                e.getTextChannel().sendMessage(e.getMessageEditor().getMessage(MessageEditor.MessageType.INFO)
-                        .setAuthor(e.getMessageEditor().getTerm("commands.nsfw.gif.error.title"), url.replace("\"}", ""))
-                        .setImage(url.replace("\"}", ""))
-                        .setFooter("Erotic")
-                        .build()).queue();
-
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
+            e.getTextChannel().sendMessage(e.getMessageEditor().getMessage(MessageEditor.MessageType.INFO)
+                    .setAuthor(e.getMessageEditor().getTerm("commands.nsfw.gif.error.title"), url)
+                    .setImage(url)
+                    .setFooter("Erotic")
+                    .build()).queue();
 
         } else {
             e.getTextChannel().sendMessage(e.getMessageEditor().getMessage(MessageEditor.MessageType.NO_NSFW).build()).queue();
