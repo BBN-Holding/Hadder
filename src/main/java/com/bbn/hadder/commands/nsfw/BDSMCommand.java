63b9d1f96f90015d46f2c6080a003b92059a49e4
GregTCLTK <gregtcltk@gmail.com>
2020-01-20 19:15:15 +0100

    BDSM

--- src/main/java/com/bbn/hadder/commands/nsfw/BDSMCommand.java
+++ src/main/java/com/bbn/hadder/commands/nsfw/BDSMCommand.java
@@ -7,12 +7,7 @@ package com.bbn.hadder.commands.nsfw;
 import com.bbn.hadder.commands.Command;
 import com.bbn.hadder.commands.CommandEvent;
 import com.bbn.hadder.utils.MessageEditor;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import org.json.JSONObject;
-
-import java.io.IOException;
+import com.bbn.hadder.utils.Request;
 
 public class BDSMCommand implements Command {
 
@@ -20,25 +15,13 @@ public class BDSMCommand implements Command {
     public void executed(String[] args, CommandEvent e) {
         if (e.getTextChannel().isNSFW()) {
 
-            OkHttpClient caller = new OkHttpClient();
-            Request request = new Request.Builder().url("https://api.nekos.dev/api/v3/images/nsfw/img/bdsm_lewd").build();
+            String url = Request.get("https://api.nekos.dev/api/v3/images/nsfw/img/bdsm_lewd");
 
-            try {
-                Response response = caller.newCall(request).execute();
-                JSONObject json = new JSONObject(response.body().string());
-                JSONObject data = json.getJSONObject("data");
-                JSONObject response1 = data.getJSONObject("response");
-                String url = response1.toString().replace("{\"url\":\"", "");
-
-                e.getTextChannel().sendMessage(e.getMessageEditor().getMessage(MessageEditor.MessageType.INFO)
-                        .setAuthor(e.getMessageEditor().getTerm("commands.nsfw.gif.error.title"), url.replace("\"}", ""))
-                        .setImage(url.replace("\"}", ""))
-                        .setFooter("BDSM")
-                        .build()).queue();
-
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
+            e.getTextChannel().sendMessage(e.getMessageEditor().getMessage(MessageEditor.MessageType.INFO)
+                    .setAuthor(e.getMessageEditor().getTerm("commands.nsfw.gif.error.title"), url)
+                    .setImage(url)
+                    .setFooter("BDSM")
+                    .build()).queue();
 
         } else {
             e.getTextChannel().sendMessage(e.getMessageEditor().getMessage(MessageEditor.MessageType.NO_NSFW).build()).queue();
