e95dc393276a1b378dc65cfa9d71c95ca07ed5ea
GregTCLTK <gregtcltk@gmail.com>
2020-02-02 15:48:31 +0100

    Unused import

--- src/main/java/com/bbn/hadder/commands/misc/ProfileCommand.java
+++ src/main/java/com/bbn/hadder/commands/misc/ProfileCommand.java
@@ -8,7 +8,6 @@ import com.bbn.hadder.commands.Command;
 import com.bbn.hadder.commands.CommandEvent;
 import com.bbn.hadder.utils.MessageEditor;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import org.apache.commons.lang3.StringUtils;
 
