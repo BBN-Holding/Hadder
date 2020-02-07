1f3a9023d4e241a256b2a95a257f5c40c86270ee
GregTCLTK <gregtcltk@gmail.com>
2020-02-07 12:31:27 +0100

    Some better stuff

--- src/main/java/com/bbn/hadder/core/CommandHandler.java
+++ src/main/java/com/bbn/hadder/core/CommandHandler.java
@@ -76,8 +76,7 @@ public class CommandHandler {
                             }
                         }
                     }
-                    if (run)
-                        cmd.executed(args, commandEvent);
+                    if (run) cmd.executed(args, commandEvent);
                     else event.getMessage().addReaction("❌").queue();
                     return;
                 }
