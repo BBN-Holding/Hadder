85611bf0693c927ba81958ad5bca1ca7a59eecc0
GregTCLTK <gregtcltk@gmail.com>
2020-02-22 21:24:24 +0100

    idk

--- src/main/java/com/bbn/hadder/core/Config.java
+++ src/main/java/com/bbn/hadder/core/Config.java
@@ -142,7 +142,7 @@ public class Config {
     public String getDiscordExtremeListToken() {
         return config.getJSONObject("Tokens").getString("DiscordExtremeList");
     }
-    
+
     public String getDiscordBotsToken() {
         return config.getJSONObject("Tokens").getString("DiscordBots");
     }
