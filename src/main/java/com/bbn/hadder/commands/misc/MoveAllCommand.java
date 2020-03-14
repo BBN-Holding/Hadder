e031eb1532217a8b406d59988f415fabbddb05a8
GregTCLTK <gregtcltk@gmail.com>
2020-03-14 15:28:16 +0100

    Fix help stuff

--- src/main/java/com/bbn/hadder/commands/misc/MoveAllCommand.java
+++ src/main/java/com/bbn/hadder/commands/misc/MoveAllCommand.java
@@ -1,5 +1,17 @@
 /*
- * @author Hax / Hax6775 / Schlauer_Hax
+ * Copyright 2019-2020 GregTCLTK and Schlauer-Hax
+ *
+ * Licensed under the GNU Affero General Public License, Version 3.0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.gnu.org/licenses/agpl-3.0.en.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.bbn.hadder.commands.misc;
@@ -9,10 +21,7 @@ import com.bbn.hadder.commands.CommandEvent;
 import com.bbn.hadder.core.Perm;
 import com.bbn.hadder.core.Perms;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Member;
 
-import java.util.ArrayList;
 import java.util.Objects;
 
 public class MoveAllCommand implements Command {
@@ -29,24 +38,22 @@ public class MoveAllCommand implements Command {
             );
             e.getChannel().sendMessage(new EmbedBuilder().setTitle("Successfully Moved!").setDescription("I moved " +
                      count + " Members. Have fun!").build()).queue();
-        } else {
-            e.getHelpCommand().sendHelp(this, e);
-        }
+        } else e.getHelpCommand().sendHelp(this, e);
     }
 
     @Override
     public String[] labels() {
-        return new String[]{"moveall", "move-all"};
+        return new String[]{"moveall", "move-all", "ma"};
     }
 
     @Override
     public String description() {
-        return "Moves All users in channel1 to channel2";
+        return "commands.misc.moveall.help.description";
     }
 
     @Override
     public String usage() {
-        return "[channel1] [channel2]";
+        return "[source-channel] [target-channel]";
     }
 
     @Override
