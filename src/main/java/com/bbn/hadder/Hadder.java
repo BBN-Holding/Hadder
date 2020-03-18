1438fba6ba35e2e71f498975e0d8620ef821d14d
GregTCLTK <gregtcltk@gmail.com>
2020-03-18 18:38:59 +0100

    Remove deprecated function

--- src/main/java/com/bbn/hadder/Hadder.java
+++ src/main/java/com/bbn/hadder/Hadder.java
@@ -51,15 +51,14 @@ public class Hadder {
         Rethink rethink = new Rethink(config);
         rethink.connect();
 
-        DefaultShardManagerBuilder builder = new DefaultShardManagerBuilder();
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(config.getBotToken());
 
         builder.setAutoReconnect(true);
         builder.setShardsTotal(1);
-        builder.setChunkingFilter(ChunkingFilter.ALL);
+        builder.setChunkingFilter(ChunkingFilter.NONE);
         builder.setBulkDeleteSplittingEnabled(true);
         builder.setActivity(Activity.listening("to h.help"));
         builder.setStatus(OnlineStatus.DO_NOT_DISTURB);
-        builder.setToken(config.getBotToken());
 
         HelpCommand helpCommand = new HelpCommand();
         AudioManager audioManager = new AudioManager();
