3ab7d38e3a3a8a1371fd15a1aef5ed2964cca65b
GregTCLTK <gregtcltk@gmail.com>
2020-05-12 17:35:35 +0200

    GVS

--- src/main/java/com/bbn/hadder/Hadder.java
+++ src/main/java/com/bbn/hadder/Hadder.java
@@ -52,7 +52,7 @@ public class Hadder {
         Rethink rethink = new Rethink(config);
         rethink.connect();
 
-        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.create(GatewayIntent.GUILD_MEMBERS, GatewayIntent.DIRECT_MESSAGE_REACTIONS, GatewayIntent.GUILD_PRESENCES);
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.create(GatewayIntent.GUILD_MEMBERS, GatewayIntent.DIRECT_MESSAGE_REACTIONS, GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_VOICE_STATES);
 
         builder.setAutoReconnect(true);
         builder.setShardsTotal(1);
